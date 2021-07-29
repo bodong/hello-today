894e4243e5a92eae7b22898206edd5dd0c9f68d6
Student User <student@workstation.lab.example.com>
2021-07-29 04:49:54 -0400

    added jkube

--- src/main/java/edu/sew/App.java
+++ src/main/java/edu/sew/App.java
@@ -2,7 +2,6 @@ package edu.sew;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class App 
